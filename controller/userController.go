7d9c29fd21296cdd351d68d1235aead9c945a06c
zaizai <327120052@qq.com>
2023-01-21 17:06:06 +0800

    20230121 qianlinyi 用户信息接口功能完成

--- controller/userController.go
+++ controller/userController.go
@@ -6,10 +6,20 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // usersLoginInfo use map to store user info, and key is username+password for demo
 // user data will be cleared every time the server starts
+var usersLoginInfo = map[string]User{
+	"zhangleidouyin": {
+		Id:            1,
+		Name:          "zhanglei",
+		FollowCount:   10,
+		FollowerCount: 5,
+		IsFollow:      true,
+	},
+}
 
 // var userIdSequence = int64(1)
 
@@ -81,17 +91,23 @@ func Login(c *gin.Context) {
 	}
 }
 
-//func UserInfo(c *gin.Context) {
-//	token := c.Query("token")
-//
-//	if user, exist := usersLoginInfo[token]; exist {
-//		c.JSON(http.StatusOK, UserResponse{
-//			Response: Response{StatusCode: 0},
-//			User:     user,
-//		})
-//	} else {
-//		c.JSON(http.StatusOK, UserResponse{
-//			Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
-//		})
-//	}
-//}
+func UserInfo(c *gin.Context) {
+	user_id := c.Query("user_id")
+	id, _ := strconv.ParseInt(user_id, 10, 64)
+	if user, err := dao.QueryUserById(id); err != nil {
+		c.JSON(http.StatusOK, UserResponse{
+			Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
+		})
+	} else {
+		c.JSON(http.StatusOK, UserResponse{
+			Response: Response{StatusCode: 0},
+			User: User{
+				user.Id,
+				user.Username,
+				user.FollowCount,
+				user.FollowerCount,
+				user.IsFollow,
+			},
+		})
+	}
+}
--- dao/userDao.go
+++ dao/userDao.go
@@ -23,6 +23,16 @@ func QueryUserByUsername(name string) (User, error) {
 	return user, nil
 }
 
+// QueryUserById 根据用户id查询用户
+func QueryUserById(id int64) (User, error) {
+	user := User{}
+	if err := Db.Where("id = ?", id).First(&user).Error; err != nil {
+		log.Println(err.Error())
+		return user, err
+	}
+	return user, nil
+}
+
 // InsertUser 将user插入表内
 func InsertUser(user *User) bool {
 	if err := Db.Create(&user).Error; err != nil {
