f0c166739639ee2ff447390c0195f5f7eabcc88b
qianlinyi <327120052@qq.com>
2023-01-31 18:41:30 +0800

    20220131 qianlinyi 项目安全性：隐藏config.go文件

--- middleware/ftp/ftp_test.go
+++ middleware/ftp/ftp_test.go
@@ -10,8 +10,8 @@ import (
 func TestFfmpeg(t *testing.T) {
 	Init()
 	for i := 1; i <= 1; i++ {
-		name := "heibao.mp4"
-		file, err := os.Open("/home/admin/tiny-tiktok/data/" + name)
+		name := "bear.mp4"
+		file, err := os.Open("/Users/zaizai/Projects/GolandProjects/tiny-tiktok/data/" + name)
 		if err != nil {
 			t.Fatal("打开不了文件")
 		}
