654bc6e4ec9789252dc52119a8945f6a95595d35
qianlinyi <327120052@qq.com>
2023-02-18 21:15:03 +0800

    20230218 qianlinyi 添加users表和videos表索引

--- config/tableStruct.sql
+++ config/tableStruct.sql
@@ -2,87 +2,95 @@ SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for comments
--- ----------------------------
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE `comments` (
-                            `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '评论id，自增主键',
-                            `user_id` bigint(20) NOT NULL COMMENT '评论发布用户id',
-                            `video_id` bigint(20) NOT NULL COMMENT '评论视频id',
-                            `comment_text` varchar(255) NOT NULL COMMENT '评论内容',
-                            `create_date` datetime NOT NULL COMMENT '评论发布时间',
-                            PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='评论表';
-
--- ----------------------------
--- Table structure for follows
--- ----------------------------
-DROP TABLE IF EXISTS `follows`;
-CREATE TABLE `follows` (
-                           `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-                           `user_id` bigint(20) NOT NULL COMMENT '用户id',
-                           `follower_id` bigint(20) NOT NULL COMMENT '关注的用户',
-                           PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='关注表';
-
--- ----------------------------
--- Table structure for likes
--- ----------------------------
-DROP TABLE IF EXISTS `likes`;
-CREATE TABLE `likes` (
-                         `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-                         `user_id` bigint(20) NOT NULL COMMENT '点赞用户id',
-                         `video_id` bigint(20) NOT NULL COMMENT '被点赞的视频id',
-                         PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='点赞表';
-
--- ----------------------------
 -- Table structure for users
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`
 (
-    `id`       bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户id，自增主键',
+    `id`       bigint(20)   NOT NULL AUTO_INCREMENT COMMENT '用户id，自增主键',
     `name`     varchar(255) NOT NULL COMMENT '用户名',
     `password` varchar(255) NOT NULL COMMENT '用户密码',
     PRIMARY KEY (`id`),
-    KEY        `name_idx` (`name`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='用户表';
+    UNIQUE INDEX (`name`, `password`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1000
+  DEFAULT CHARSET = utf8 COMMENT ='用户表';
+
+-- ----------------------------
+-- Table structure for comments
+-- ----------------------------
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments`
+(
+    `id`           bigint(20)   NOT NULL AUTO_INCREMENT COMMENT '评论id，自增主键',
+    `user_id`      bigint(20)   NOT NULL COMMENT '评论发布用户id',
+    `video_id`     bigint(20)   NOT NULL COMMENT '评论视频id',
+    `comment_text` varchar(255) NOT NULL COMMENT '评论内容',
+    `create_date`  datetime     NOT NULL COMMENT '评论发布时间',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1000
+  DEFAULT CHARSET = utf8 COMMENT ='评论表';
+
+-- ----------------------------
+-- Table structure for follows
+-- ----------------------------
+DROP TABLE IF EXISTS `follows`;
+CREATE TABLE `follows`
+(
+    `id`          bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+    `user_id`     bigint(20) NOT NULL COMMENT '用户id',
+    `follower_id` bigint(20) NOT NULL COMMENT '关注的用户',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1000
+  DEFAULT CHARSET = utf8 COMMENT ='关注表';
+
+-- ----------------------------
+-- Table structure for likes
+-- ----------------------------
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE `likes`
+(
+    `id`       bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+    `user_id`  bigint(20) NOT NULL COMMENT '点赞用户id',
+    `video_id` bigint(20) NOT NULL COMMENT '被点赞的视频id',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1000
+  DEFAULT CHARSET = utf8 COMMENT ='点赞表';
 
 -- ----------------------------
 -- Table structure for videos
 -- ----------------------------
 DROP TABLE IF EXISTS `videos`;
-CREATE TABLE `videos` (
-                          `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键，视频唯一id',
-                          `author_id` bigint(20) NOT NULL COMMENT '视频作者id',
-                          `play_url` varchar(255) NOT NULL COMMENT '播放url',
-                          `cover_url` varchar(255) NOT NULL COMMENT '封面url',
-                          `publish_time` datetime NOT NULL COMMENT '发布时间戳',
-                          `title` varchar(255) DEFAULT NULL COMMENT '视频名称',
-                          PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='\r\n视频表';
+CREATE TABLE `videos`
+(
+    `id`           bigint(20)   NOT NULL AUTO_INCREMENT COMMENT '自增主键，视频唯一id',
+    `author_id`    bigint(20)   NOT NULL COMMENT '视频作者id',
+    `play_url`     varchar(255) NOT NULL COMMENT '播放url',
+    `cover_url`    varchar(255) NOT NULL COMMENT '封面url',
+    `publish_time` datetime     NOT NULL COMMENT '发布时间戳',
+    `title`        varchar(255) DEFAULT NULL COMMENT '视频名称',
+    PRIMARY KEY (`id`),
+    INDEX (`author_id`),
+    INDEX (`publish_time`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1000
+  DEFAULT CHARSET = utf8 COMMENT ='\r\n视频表';
 
 -- ----------------------------
 -- Table structure for messages
 -- ----------------------------
 DROP TABLE IF EXISTS `messages`;
-CREATE TABLE `messages` (
-                            `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '消息id，自增主键',
-                            `to_user_id` bigint(20) NOT NULL COMMENT '接收用户id',
-                            `from_user_id` bigint(20) NOT NULL COMMENT '发送用户id',
-                            `content` varchar(255) NOT NULL COMMENT '消息内容',
-                            `create_time` varchar(255) NOT NULL COMMENT '消息发送时间',
-                            PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='消息表';
-
--- DROP TABLE IF EXISTS `messages`;
--- CREATE TABLE `messages` (
---   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '消息id，自增主键',
--- 	`message_key` varchar(255) NOT NULL COMMENT '消息key',
---   `content` varchar(255) NOT NULL COMMENT '消息内容',
---   `create_time` datetime NOT NULL COMMENT '消息发送时间',
---   PRIMARY KEY (`id`)
--- ) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='消息表';
-
-SET FOREIGN_KEY_CHECKS = 1;
+CREATE TABLE `messages`
+(
+    `id`           bigint(20)   NOT NULL AUTO_INCREMENT COMMENT '消息id，自增主键',
+    `to_user_id`   bigint(20)   NOT NULL COMMENT '接收用户id',
+    `from_user_id` bigint(20)   NOT NULL COMMENT '发送用户id',
+    `content`      varchar(255) NOT NULL COMMENT '消息内容',
+    `create_time`  varchar(255) NOT NULL COMMENT '消息发送时间',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1000
+  DEFAULT CHARSET = utf8 COMMENT ='消息表';
