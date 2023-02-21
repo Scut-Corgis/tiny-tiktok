12185a76964cae4e18f4231b18e405851322313e
weipengyan <weipengyan1123@163.com>
2023-02-21 20:30:52 +0800

    tableStruct.sql likes表添加索引 修改messages表

--- config/tableStruct.sql
+++ config/tableStruct.sql
@@ -58,6 +58,9 @@ CREATE TABLE `likes`
     `user_id`  bigint(20) NOT NULL COMMENT '点赞用户id',
     `video_id` bigint(20) NOT NULL COMMENT '被点赞的视频id',
     PRIMARY KEY (`id`)
+    UNIQUE KEY `userIdtoVideoIdIdx` (`user_id`,`video_id`) USING BTREE,
+    KEY `userIdIdx` (`user_id`) USING BTREE,
+    KEY `videoIdx` (`video_id`) USING BTREE
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 1000
   DEFAULT CHARSET = utf8 COMMENT ='点赞表';
